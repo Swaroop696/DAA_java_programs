1f15fe53312f25aa525b8ce7283a064a2f91d162
Swaroop K S <80799982+Swaroop696@users.noreply.github.com>
2022-08-04 16:13:08 +0530

    Add files via upload

---
+++ Floyds.java
@@ -0,0 +1,63 @@
+
+package hi;
+import java.util.*;
+public class Hi {
+
+    public static int min(int a,int b)
+    {
+        if(a>b)
+        {
+            return b;
+        }
+        return a;
+    }
+    
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        int n;
+        System.out.println("Enter the number of vertices");
+        n=scan.nextInt();
+        int adj[][]=new int[n][n];
+        System.out.println("Enter the adjascency matrix");
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                adj[i][j]=scan.nextInt();
+            }
+        }
+        
+        System.out.println("Entered adjascency matrix");
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                System.out.print(adj[i][j]+" ");
+            }System.out.println();
+        }
+        
+        
+        for(int k=0;k<n;k++)
+        {
+            for(int i=0;i<n;i++)
+            {
+                for(int j=0;j<n;j++)
+                {
+                    adj[i][j]=min( adj[i][j], (adj[i][k]+adj[k][j]));
+                }
+            }
+        }
+        
+        System.out.println("The final matrix is");
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                System.out.print(adj[i][j]+" ");
+            }System.out.println();
+        }
+        
+        
+    }
+    
+}
